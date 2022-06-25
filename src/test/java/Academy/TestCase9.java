195192555bc8cc3f9b9bc750b796ac465b255b9d
Praveen <otty.praveen@gmail.com>
2022-06-25 23:35:44 +0530

    This is second changes

---
+++ src/test/java/Academy/TestCase9.java
@@ -0,0 +1,15 @@
+package Academy;
+
+public class TestCase9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Few Steps are added");
+		System.out.println("changed");
+		System.out.println("changed2");
+		System.out.println("changed3s");
+
+
+	}
+
+}
---
+++ src/test/java/Academy/testCase8.java
@@ -0,0 +1,14 @@
+package Academy;
+
+public class testCase8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Few Steps are added");
+		System.out.println("changed");
+		System.out.println("changed2");
+		System.out.println("changed3s");
+
+	}
+
+}
